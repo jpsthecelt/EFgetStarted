0d0e1040d4c59e59e747905dcd7a0c4b60d1f817
jpsthecelt <jpsthecelt@gmail.com>
2021-05-05 17:20:54 -0700

    incept; from tutorial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        private static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                // Note: This sample requires the database to be created before running.
+                //  (usu. done with migration)
+
+                // Create / Saving Data
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read / Querying
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+        }
+    }
+}
